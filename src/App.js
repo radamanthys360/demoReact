67658d485daa1cc93628aa2cf5e5765ab01a0383
radamanthys360 <coopervisual@hotmail.es>
2020-09-28 17:45:47 +0200

    inicio crud

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import Taringuero from './Componentes/Taringuero';
 
 class App extends Component {
-  state = {
-    taringos: [
-      { nombre: 'radamantis17', edad: 29 },
-      { nombre: 'virgoconmani', edad: 25 },
-      { nombre: 'la polaca', edad: 35 }
-    ]
-  };
-
-  switchNombreHandler = () => {
-    this.setState({
-      taringos: [
-        { nombre: 'radamantis17', edad: 29 },
-        { nombre: 'El facha', edad: 25 },
-        { nombre: 'la polaca', edad: 35 }
-      ]
-    });
-  };
 
   render() {
     return (
-      <div className="App">
-        <button onClick={this.switchNombreHandler}>Cambiar Taringuero</button>
-        <Taringuero
-          nombre={this.state.taringos[0].nombre}
-          edad={this.state.taringos[0].edad}
-        />
-        <Taringuero
-          nombre={this.state.taringos[1].nombre}
-          edad={this.state.taringos[1].edad}
-        >Todas putas menos conmigo!!</Taringuero>
-        <Taringuero
-          nombre={this.state.taringos[2].nombre}
-          edad={this.state.taringos[2].edad}
-        />
-      </div>
+        <h1>
+          es una prueba
+        </h1>
     );
   }
 }
